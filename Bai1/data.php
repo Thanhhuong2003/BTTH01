59038ff3452b10d29f75131b84214d6c5f15c064
Thanhhuong2003 <tranhuong30092003@gmail.com>
2024-11-30 00:37:19 +0700

    Thêm bài thực hành 1

---
+++ Bai1/data.php
@@ -0,0 +1,88 @@
+<?php
+$flowers = 
+[
+[
+    'stt' => '1',
+    'ten' => ' Hoa dạ yến thảo',
+    'mo ta'=> 'Nở rực rỡ quanh năm kể cả thời tiết xe lạnh ',
+    'image' => 'dayenthao.jpg'
+],
+[   'stt'=> '2',
+    'ten'=>'Hoa đồng tiền',
+    'mo ta'=> 'Có hoa to nở rực rỡ, thích hợp trồng cuối xuân đầu hè',
+    'image'=> 'dongtien.jpg'
+
+],
+[
+    'stt'=>'3',
+    'ten'=>'Hoa giấy',
+    'mo ta'=>'Mỏng manh nhưng rất lâu tàn có rất nhiều màu',
+    'image'=>'giay.jpg'
+],
+[
+    'stt'=>'4',
+    'ten'=>'Hoa thanh tú',
+    'mo ta'=>'Dáng hình tao nhã, màu sắc thiên thanh dịu dàng,thích hợp cả xuân lẫn hè',
+    'image'=> 'thanhtu.jpg'
+],
+['stt'=>'5',
+    'ten'=>'Hoa đèn lồng',
+    'mo ta'=>'Giống đèn lồng, nó còn có tên là hồng đăng hoa',
+    'image'=> 'denlong.jpg'
+],
+[
+    'stt'=>'6',
+    'ten'=>'Hoa cẩm chướng',
+    'mo ta'=>'Thân mảnh, các đốt ngắn, mang lá kép cùng nhiều màu sắc',
+    'image'=> 'camchuong.jpg',
+],
+[
+    'stt'=>'7',
+    'ten'=>'Hoa huỳnh anh ',
+    'mo ta'=>'Có màu vàng rực, hình dạng như chiếc kèn be bé, dễ trồng, mọc nhanh',
+    'image'=> 'huynhanh.jpg',
+],
+[
+    'stt'=>'8',
+    'ten'=>'Hoa păng-xê',
+    'mo ta'=>'Cánh mòng mượt như nhung, hình dạng cánh bướm còn được gọi là hoa tử la lan',
+    'image'=> 'pãnge.jpg',
+],
+[
+    'stt'=>'9',
+    'ten'=>'Hoa sen',
+    'mo ta'=>'Tượng trưng cho vẻ đẹp trắng trong, tao nhã, thường có vào mùa hè',
+    'image'=> 'sen.jpg',
+],
+[
+    'stt'=>'10',
+    'ten'=>'Hoa dừa cạn',
+    'mo ta'=>'Tượng trưng cho sự thành đạt và có khả năng trừ tà, còn có tên là hoa hải đằng',
+    'image'=> 'duacan.jpg',
+],
+[
+    'stt'=>'11',
+    'ten'=>'Hoa sử quân tử',
+    'mo ta'=>'Có mùi hương rất thơm, có màu pha trăng hoặc đỏ tươi ',
+    'image'=> 'suquantu.jpg',
+],
+[
+    'stt'=>'12',
+    'ten'=>'Hoa cúc lá nho',
+    'mo ta'=>'Có màu sắc rất phong phú, lá to và có răng cưa',
+    'image'=> 'cuclanho',
+],
+[
+    'stt'=>'13',
+    'ten'=>'Hoa cẩm tú cầu',
+    'mo ta'=>'Mọc thành bụi, hoa nở thành từng chùm, tàn khi mặt trời lặn',
+    'image'=> 'camtucau.jpg',
+],
+[
+    'stt'=>'14',
+    'ten'=>'Hoa cúc dại',
+    'mo ta'=>'Thân mảnh, mọc cả khi thời tiết khắc nghiệt',
+    'image'=> 'cucdai.jpg',
+],
+    
+];
